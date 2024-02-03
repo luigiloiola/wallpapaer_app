91415513a6cd98b1f211a585194191bb88ac8109
luigiloiola <60331397+luigiloiola@users.noreply.github.com>
2024-02-03 19:55:48 -0300

    estrutura inicial do projeto

--- groups/serializers.py
+++ groups/serializers.py
@@ -7,4 +7,4 @@ class GroupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Group
-        fields = ['id', 'createdBy']
+        fields = ['id', 'createdBy', 'currentWallpaper']
