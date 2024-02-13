58f4fe375b40d49418a6351a3500d721726a0673
luigiloiola <60331397+luigiloiola@users.noreply.github.com>
2024-02-13 20:32:44 -0300

    ajuste config

--- wallpaper_app/wsgi.py
+++ wallpaper_app/wsgi.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wallpaper_app.settings')
-
-app = get_wsgi_application()
+application = get_wsgi_application()
