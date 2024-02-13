505b928a024e9df82c940fb3aed568d0cd30db42
luigiloiola <60331397+luigiloiola@users.noreply.github.com>
2024-02-13 12:42:07 -0300

    ajuste no endereço

--- vercel_app/asgi.py
+++ vercel_app/asgi.py
@@ -11,7 +11,7 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from django.urls import re_path
+from django.urls import re_path, path
 from . import consumers
 
 
@@ -22,7 +22,7 @@ application = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(
         URLRouter(
             [
-                re_path(r"ws/loiola-app.vercel.app/<str:room_name>/", consumers.WallpaperConsumer.as_asgi()),
+                path(r"ws/<str:room_name>/", consumers.WallpaperConsumer.as_asgi()),
             ]
         )
     ),
