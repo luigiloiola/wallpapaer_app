a2900ed9505df73d654a1d000aa289d57f3c2019
luigiloiola <60331397+luigiloiola@users.noreply.github.com>
2024-02-14 02:52:10 -0300

    ajuste config

--- groups/models.py
+++ groups/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 
 class Group(models.Model):
     createdBy = models.ForeignKey(User, on_delete=models.CASCADE)
-    currentWallpaper = models.CharField(max_length=255, blank=True, null=True)
+    currentWallpaper = models.CharField(max_length=254, blank=True, null=True)
     # Adicione outros campos conforme necessário
