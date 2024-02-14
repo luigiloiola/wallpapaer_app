0832485c6d2f0ad52bc71e85ce2bbb6b4f5f9f9b
luigiloiola <60331397+luigiloiola@users.noreply.github.com>
2024-02-13 21:07:38 -0300

    ajuste config

--- wallpaper_app/settings.py
+++ wallpaper_app/settings.py
@@ -91,12 +91,8 @@ WSGI_APPLICATION = 'wallpaper_app.wsgi.app'
 # Note: Django modules for using databases are not support in serverless
 # environments like Vercel. You can use a database over HTTP, hosted elsewhere.
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+
 
 CHANNEL_LAYERS = {
     "default":{
