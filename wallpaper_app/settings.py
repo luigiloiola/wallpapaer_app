6be5b61c12250df149680d14f89356c9fb4b6f07
luigiloiola <60331397+luigiloiola@users.noreply.github.com>
2024-02-14 02:43:13 -0300

    ajuste config

--- wallpaper_app/settings.py
+++ wallpaper_app/settings.py
@@ -96,23 +96,9 @@ WSGI_APPLICATION = 'wallpaper_app.wsgi.app'
 # Note: Django modules for using databases are not support in serverless
 # environments like Vercel. You can use a database over HTTP, hosted elsewhere.
 
-if IS_HEROKU_APP:
-    # In production on Heroku the database configuration is derived from the `DATABASE_URL`
-    # environment variable by the dj-database-url package. `DATABASE_URL` will be set
-    # automatically by Heroku when a database addon is attached to your Heroku app. See:
-    # https://devcenter.heroku.com/articles/provisioning-heroku-postgres
-    # https://github.com/jazzband/dj-database-url
-    DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
-else:
-    # When running locally in development or in CI, a sqlite database file will be used instead
-    # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+
 
 
 CHANNEL_LAYERS = {
